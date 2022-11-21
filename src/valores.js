9baac8e1a0fba8cb16c26d6157af8d3045ecf5cd
David Mecalco Alejo <66454464+DavidMecalco@users.noreply.github.com>
2022-11-21 17:36:52 -0600

    Agregamos el archivo valores.js

---
+++ src/valores.js
@@ -0,0 +1,23 @@
+//Para valores de tipo número:
+40
+
+//Para valores de tipo texto 
+"David Mecalco Alejo"
+
+//Valor booleano
+true 
+false
+
+//Valores vacios o valores placeHolder
+null
+
+//Para undefined es para un valor o campo que se va a utilizar despues
+undefined
+
+//Valores de tipos objetos
+[1, 2, 3]
+
+//Valor de tipo objeto con corchetes
+{nombre: "David Mecalco"}
+
+
