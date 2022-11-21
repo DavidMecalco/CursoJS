51c505c47521796a1f2303d3fdfd381ad3c151c0
David Mecalco Alejo <66454464+DavidMecalco@users.noreply.github.com>
2022-11-21 17:45:54 -0600

    Agregamos el archivo variables.js

---
+++ src/variables.js
@@ -0,0 +1,16 @@
+//Para inicializar una varible, debemos utilziar la palabra reservada var
+var nombre = "David Mecalco";
+
+//Estados de una varibale
+//Declarar una varible
+var edad;
+//Inicializar una variable
+edad = 25;
+
+//varibles de tipo objeto
+var elementos = ["Computadora", "Celular", "Tableta"];
+
+var persona = {
+    nombre: "David",
+    edad: 25
+}
